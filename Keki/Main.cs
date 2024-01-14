e2996baba73f3244fcbdbba2b8ced68f3656d1a4
Nurdoidz <nurdoidz@live.com>
2024-01-13 23:05:53 -0700

    Make the app

--- Keki/Main.cs
+++ Keki/Main.cs
@@ -1,26 +1,144 @@
-﻿using System.CommandLine;
+﻿//          ╒═════════════════════════════════════════════════════════╕
+//          │                          Keki                           │
+//          │           A CLI application to manage stacks            │
+//          ╘═════════════════════════════════════════════════════════╛
+using System.CommandLine;
 
-namespace scl;
+string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Keki", "cake.txt");
 
-class Main {
-    static async Task<int> Main(string[] args) {
-        var fileOption = new Option<FileInfo?>(
-            name: "--file",
-            description: "The file to read and display on the console.");
+var rootCommand = new RootCommand("Keki: Stack the cake, bake it.");
 
-        var rootCommand = new RootCommand("Sample app for System.CommandLine");
-        rootCommand.AddOption(fileOption);
+var nameArgument = new Argument<string?>(
+        name: "name",
+        description: "Name of the layer.");
 
-        rootCommand.SetHandler((file) => {
-            ReadFile(file!);
-        },
-            fileOption);
+var clearCommand = new Command("clear", "Eat the cake.");
+clearCommand.SetHandler(ClearItems);
+clearCommand.AddAlias("eat");
+rootCommand.Add(clearCommand);
 
-        return await rootCommand.InvokeAsync(args);
+var listCommand = new Command("list", "List all layers in the cake.");
+listCommand.SetHandler(ListItems);
+rootCommand.Add(listCommand);
+
+var popCommand = new Command("pop", "Eat the top layer of the cake.");
+popCommand.SetHandler(PopItem);
+rootCommand.Add(popCommand);
+
+var pushCommand = new Command("push", "Add a layer onto the cake.");
+pushCommand.AddArgument(nameArgument);
+pushCommand.SetHandler((name) => PushItem(name!), nameArgument);
+rootCommand.Add(pushCommand);
+
+rootCommand.Handler = listCommand.Handler;
+
+return await rootCommand.InvokeAsync(args);
+
+void ClearItems() {
+    File.WriteAllText(path, "");
+    Icing("No more layers in the cake.", ConsoleColor.Green);
+}
+
+void ListItems() {
+    if (!File.Exists(path)) {
+        Icing("No layers in the cake.", ConsoleColor.Red);
+        return;
     }
 
-    static void ReadFile(FileInfo file) {
-        File.ReadLines(file.FullName).ToList()
-            .ForEach(line => Console.WriteLine(line));
+    Stack<string> cake = GetTheCake();
+
+    if (cake.Count == 0) {
+        Icing("No layers in the cake.", ConsoleColor.Red);
+        return;
+    }
+
+    IceTheCake(cake);
+}
+
+void PopItem() {
+    if (!File.Exists(path)) {
+        Icing("No layers in the cake.", ConsoleColor.Red);
+        return;
+    }
+
+    Stack<string> cake = GetTheCake();
+
+    if (cake.TryPop(out var item)) {
+        Icing($"Popped \"{item}\".", ConsoleColor.Green);
+
+        if (cake.Count == 0) {
+            Icing("No more layers in the cake.", ConsoleColor.Yellow);
+        }
+
+        IceTheCake(cake);
+
+    }
+    else {
+        Icing("No layers in the cake.", ConsoleColor.Red);
+    }
+
+    BakeTheCake(cake);
+}
+
+void PushItem(string? name) {
+    Stack<string> cake = GetTheCake();
+
+    if (name == null) {
+        Console.Write("Item name: ");
+        string newName = Console.ReadLine();
+
+        if (newName != null && newName.Length > 0) name = newName;
+        else {
+            Icing("Cancelled.", ConsoleColor.Red);
+            IceTheCake(cake);
+            return;
+        }
+    }
+
+    cake.Push(name);
+    IceTheCake(cake);
+    BakeTheCake(cake);
+}
+
+void IceTheCake(Stack<string> cake) {
+    bool isFirst = true;
+    foreach (string layer in cake) {
+        if (isFirst) {
+            Icing($"{layer} <--", ConsoleColor.Cyan);
+            isFirst = false;
+        }
+        else Console.WriteLine(layer);
     }
 }
+
+void BakeTheCake(Stack<string> cake) {
+    _ = Directory.CreateDirectory(Path.GetDirectoryName(path));
+    using StreamWriter writer = new(path);
+    while (cake.Count > 0) {
+        writer.WriteLine(cake.Pop());
+    }
+}
+
+Stack<string> GetTheCake() {
+    Stack<string> reversed = new();
+    if (File.Exists(path)) {
+        foreach (string line in File.ReadLines(path)) {
+            reversed.Push(line);
+        }
+    }
+
+    Stack<string> cake = new();
+    while (reversed.Count > 0) {
+        cake.Push(reversed.Pop());
+    }
+
+    return cake;
+}
+
+void Icing(string line, ConsoleColor color) {
+    Console.ForegroundColor = color;
+    Console.WriteLine(line);
+    Console.ResetColor();
+}
+
+
